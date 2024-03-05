109342bde98770df320761d26161fe55cb714173
uo288120 <145131850+Pedro-C-M@users.noreply.github.com>
2024-03-05 19:33:16 +0100

    Contenedor centrado para poner la pregunta

--- webapp/pruebas-vite/src/App.jsx
+++ webapp/pruebas-vite/src/App.jsx
@@ -1,17 +1,27 @@
 import './App.css';
 import { AnswerButton } from './components/AnswerButton.jsx';
-import { Box } from "@chakra-ui/react";
+import { Box,Center } from "@chakra-ui/react";
 
 
 export function App() {
   return (
     <>
       <Box minH="100vh" minW="100vw" 
-      bgGradient="linear(to-t,#08313A,#107869)">
-        <AnswerButton text={"Primera pregunta"} colorFondo={"#A06AB4"}/>
-        <AnswerButton text={"Segunda pregunta"} colorFondo={"#B5EECB"}/>
-        <AnswerButton text={"Tercera pregunta"} colorFondo={"#FFD743"}/>
-        <AnswerButton text={"Cuarta pregunta"} colorFondo={"#D773A2"}/>
+      bgGradient="linear(to-t,#08313A,#107869)"
+      display="flex" justifyContent="center" alignItems="center">
+        <Box alignContent="center" bg="#0000004d" 
+        maxH="80vh" maxW="70vW" minH="70vh" minW="60vW">
+          <Center borderBottom="0.1em solid #000" 
+            height="3">
+              Pregunta
+          </Center>
+          <Box>
+            <AnswerButton text={"Primera pregunta"} colorFondo={"#A06AB4"}/>
+            <AnswerButton text={"Segunda pregunta"} colorFondo={"#B5EECB"}/>
+            <AnswerButton text={"Tercera pregunta"} colorFondo={"#FFD743"}/>
+            <AnswerButton text={"Cuarta pregunta"} colorFondo={"#D773A2"}/>
+          </Box>
+        </Box>
       </Box>
     </>
   )

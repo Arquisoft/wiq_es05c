7bcb9999d3660a2c385c0b15def6bf2c7b5d23f5
uo288120 <145131850+Pedro-C-M@users.noreply.github.com>
2024-03-05 20:16:20 +0100

    Pasados a componentes código en App.jsx

--- webapp/pruebas-vite/src/App.jsx
+++ webapp/pruebas-vite/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import { AnswerButton } from './components/AnswerButton.jsx';
-import { Box,Center } from "@chakra-ui/react";
+import { Box} from "@chakra-ui/react";
+import { QuestionArea } from './components/QuestionArea';
 
 
 export function App() {
@@ -9,19 +9,7 @@ export function App() {
       <Box minH="100vh" minW="100vw" 
       bgGradient="linear(to-t,#08313A,#107869)"
       display="flex" justifyContent="center" alignItems="center">
-        <Box alignContent="center" bg="#0000004d" display="flex" flexDir="column"
-        maxH="80vh" maxW="70vW" minH="70vh" minW="60vW">
-          <Center borderBottom="0.1em solid #000" fontSize="1.5em" color="#FCFAF0" fontWeight="bolder"
-            flex="2">
-              Pregunta
-          </Center>
-          <Box display="grid" flex="1" gridTemplateColumns="repeat(2,1fr)" gridColumnGap="2em" padding="4em" alignItems="center">
-            <AnswerButton text={"Primera pregunta"} colorFondo={"#A06AB4"}/>
-            <AnswerButton text={"Segunda pregunta"} colorFondo={"#B5EECB"}/>
-            <AnswerButton text={"Tercera pregunta"} colorFondo={"#FFD743"}/>
-            <AnswerButton text={"Cuarta pregunta"} colorFondo={"#D773A2"}/>
-          </Box>
-        </Box>
+        <QuestionArea/>
       </Box>
     </>
   )

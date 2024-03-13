9974b2fbb37b2ed21ee915e7e95fa67072cce4c1
uo288120 <145131850+Pedro-C-M@users.noreply.github.com>
2024-03-13 18:47:33 +0100

    Los test los carga el diablo

---
+++ webapp/src/components/AnswerButton.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import AnswerButton from './AnswerButton';
+import { ChakraProvider } from '@chakra-ui/react';
+
+
+describe('AnswerButton component', () => {
+  it('should render correctly', () => {
+    // // Arrange
+    // const text = 'Answer';
+    // const colorFondo = '#A06AB4';
+    // const onClick = jest.fn();
+
+    // Act
+    render(
+      <ChakraProvider>
+        <AnswerButton></AnswerButton>
+      </ChakraProvider>
+    );
+    // const buttonElement = screen.getByText(text);
+
+    // // Assert
+    // expect(buttonElement).toBeInTheDocument();
+    // expect(buttonElement).toHaveStyle(`background-color: ${colorFondo}`);
+  });
+
+  // it('should call onClick when clicked', () => {
+  //   // Arrange
+  //   const text = 'Answer';
+  //   const colorFondo = '#A06AB4';
+  //   const onClick = jest.fn();
+
+  //   // Act
+  //   const { getByText } = render(
+  //     <AnswerButton text={text} colorFondo={colorFondo} onClick={onClick} />
+  //   );
+  //   const buttonElement = getByText(text);
+  //   fireEvent.click(buttonElement);
+
+  //   // Assert
+  //   expect(onClick).toHaveBeenCalledTimes(1);
+  // });
+});

976842cd8498f1ba8a0ad8dbef81c02be7cf2fe8
lucia-sonia <uo282189@uniovi.es>
2024-03-20 20:01:54 +0100

    lucia-sonia agregado en el gateway service la nueva url y actualizado el fichero API preguntas

--- gatewayservice/gateway-service.js
+++ gatewayservice/gateway-service.js
@@ -54,6 +54,18 @@ app.get('/getQuestion', async (req, res) => {
   }
 });
 
+app.get('/getQuestionModoBasico', async (req, res) => {
+  try {
+    // llamamos al servicio de preguntas
+    const questionResponse = await axios.get(questionServiceUrl+'/getQuestionModoBasico', req.body);
+    
+    res.json(questionResponse.data);
+  } catch (error) {
+    //Modifico el error 
+    res.status(500).json({ error: 'Error al realizar la solicitud al servicio de preguntas' });
+  }
+});
+
 app.get('/generateQuestions', async (req, res) => {
   try {
     // llamamos al servicio de preguntas

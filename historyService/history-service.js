071dbfb1fa9226a05c06f93f017ae4edd5064b69
lucia-sonia <uo282189@uniovi.es>
2024-03-21 10:38:56 +0100

    lucia-sonia agragado el cokerfile y el package.json al historyservice y volvimos a añadir el model al question-service

--- historyService/history-service.js
+++ historyService/history-service.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const Model = require('./history-model')
 
 const History = require("./obtenerDetallesUsuarioBaseDatos");
 const history = new History();

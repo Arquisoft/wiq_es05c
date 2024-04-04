b200a9e6953352fb8f8b0337d268083be3e50992
lucia-sonia <uo282189@uniovi.es>
2024-04-04 17:15:00 +0200

    añadida internacionalizacion en el navbar

--- webapp/src/components/navbar/NavBar.js
+++ webapp/src/components/navbar/NavBar.js
@@ -1,9 +1,12 @@
 import React, { useContext, useState } from 'react';
-import { AppBar, Toolbar, Button, IconButton, Avatar, Menu, MenuItem, Typography,ListItem, Switch } from '@mui/material';
+import { AppBar, Toolbar, Button, IconButton, Avatar, Menu, MenuItem, Typography,ListItem, ListItemIcon , Switch } from '@mui/material';
 import { styled } from '@mui/system';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../authcontext';
 
+//para la internacionalización
+import { useTranslation } from 'react-i18next';
+import LanguageIcon from '@mui/icons-material/Language';
 
 const CustomSwitch = styled(Switch)({
   '& .MuiSwitch-switchBase.Mui-checked': {
@@ -40,20 +43,60 @@ const NavBar = ({ setDarkMode, darkMode}) => {
   let barBackgroundColor = darkMode?"#001c17" : "#fef5c6";
   let textColor = darkMode?"#FCFAF0" : "#08313A";
 
+  //para la internacionalización
+  const { t, i18n } = useTranslation();
+  const [anchorLanguage, setAnchorLanguage] = useState(null);
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+    handleCloseLanguage(); 
+  };
+  
+  const handleClickLanguage = (event) => {
+    setAnchorLanguage(event.currentTarget);
+  };
+
+  const handleCloseLanguage = () => {
+    setAnchorLanguage(null);
+  };
+
 
   console.log('isLoggedIn', isLoggedIn);
   return (
     <AppBar position="static" style={{ backgroundColor: barBackgroundColor, color: textColor,  borderBottom: '0.1em solid' + textColor}}  >
       <Toolbar>
         <Button color="inherit" component={Link} to="/home">
-          Home
+          {t('home')}
         </Button>
+
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} />
+
+        <IconButton onClick={handleClickLanguage}>
+          <LanguageIcon />
+          <Typography>{i18n.language.toUpperCase()}</Typography>
+        </IconButton>
+        <Menu
+          anchorEl={anchorLanguage}
+          open={Boolean(anchorLanguage)}
+          onClose={handleCloseLanguage}
+        >
+          <MenuItem onClick={() => changeLanguage('es')}>
+            <ListItemIcon>
+              <img src="/spain_flag.png" alt={t('spanishFlag')} />
+            </ListItemIcon> 
+            {t('spanish')}</MenuItem>
+          <MenuItem onClick={() => changeLanguage('en')} startIcon={<img src="/public/uk_flag.png" alt="Spanish Flag" />}>
+          <ListItemIcon>
+            <img src="/uk_flag.png" alt={t('englishFlag')} />
+          </ListItemIcon>            
+            {t('english')}</MenuItem>
+        </Menu>
+
         <CustomSwitch onChange={handleToggle} />
         {isLoggedIn() ? (
          <>
          <IconButton onClick={handleClick}>
-           <Avatar alt="User Avatar" src="/public/icon.jpg" />
+           <Avatar alt="User Avatar" src="/icon.jpg" />
          </IconButton>
          <Menu
            anchorEl={anchorEl}
@@ -64,23 +107,24 @@ const NavBar = ({ setDarkMode, darkMode}) => {
              <Typography fontWeight="bold">{username}</Typography>
            </ListItem>
            <MenuItem onClick={handleClose} component={Link} to="/logout">
-             Cerrar sesión
+            {t('logout')}
            </MenuItem>
            <MenuItem onClick={handleClose} component={Link} to="/history">
-             Historial
+             {t('history')}
            </MenuItem>
          </Menu>
        </>
         ) : (
           <>
             <Button color="inherit" component={Link} to="/login">
-              Iniciar sesión
+              {t('signIn')}
             </Button>
             <Button color="inherit" component={Link} to="/adduser">
-              Registrarse
+              {t('signUp')}
             </Button>
           </>
         )}
+
       </Toolbar>
     </AppBar>
   );

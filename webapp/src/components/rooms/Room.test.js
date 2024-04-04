0761a2d3b4cbda5ecab45883d449306115547623
bidof <uo283196@uniovi.es>
2024-04-04 19:49:34 +0200

    test de room completos

--- webapp/src/components/rooms/Room.test.js
+++ webapp/src/components/rooms/Room.test.js
@@ -2,13 +2,20 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import Room from './Room';
-
+import socket from './socket';
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useParams: () => ({ roomId: '1234' }),
   useNavigate: () => jest.fn(),
   useLocation: () => ({ state: { isHost: true } }),
 }));
+jest.mock('./socket', () => {
+    return {
+      emit: jest.fn(),
+      on: jest.fn(),
+      off: jest.fn(),
+    };
+  });
 
 test('renders Room component', () => {
   render(
@@ -31,3 +38,41 @@ test('renders Room component with correct room ID', () => {
 
   expect(screen.getByText('Sala: 1234')).toBeInTheDocument();
 });
+
+test('renders Room component with users', () => {
+    socket.on.mockImplementation((event, callback) => {
+      if (event === 'currentUsers') {
+        callback({ 'user1': true, 'user2': true });
+      }
+    });
+  
+    render(
+      <Router>
+        <ChakraProvider>
+          <Room darkMode={false} />
+        </ChakraProvider>
+      </Router>
+    );
+  
+    expect(screen.getByText('user1')).toBeInTheDocument();
+    expect(screen.getByText('user2')).toBeInTheDocument();
+  });
+
+  test('renders Room component with users', () => {
+    socket.on.mockImplementation((event, callback) => {
+      if (event === 'currentUsers') {
+        callback({ 'user1': true, 'user2': true });
+      }
+    });
+  
+    render(
+      <Router>
+        <ChakraProvider>
+          <Room darkMode={false} />
+        </ChakraProvider>
+      </Router>
+    );
+  
+    expect(screen.getByText('user1')).toBeInTheDocument();
+    expect(screen.getByText('user2')).toBeInTheDocument();
+  });

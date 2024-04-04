dfc10bf9991efa66f9e649151a484575a1fd6e4f
lucia-sonia <uo282189@uniovi.es>
2024-04-04 16:34:10 +0200

    preparando las cosas para la internacionalizacion

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -5,11 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ColorModeScript } from "@chakra-ui/react";
 
+//para internacionalizacion
+import { I18nextProvider } from 'react-i18next';
+import i18n from './internacionalizacion/i18n.js';
 
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript initialColorMode="light" />
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>,
     
   </React.StrictMode>,
   document.getElementById('root')

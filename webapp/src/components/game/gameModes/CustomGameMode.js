1d51e34ebb0d4501bdaf972d5c29d18d23408dbc
UO283535 <uo283535@uniovi.es>
2024-04-09 16:53:39 +0200

    Se añade la clase CustomGameMode como nuevo modo de juego para probar

---
+++ webapp/src/components/game/gameModes/CustomGameMode.js
@@ -0,0 +1,24 @@
+const { default: BasicGame } = require("../BasicGame");
+const { default: GameMode } = require("./GameMode");
+
+class CustomGameMode extends BasicGame{
+    constructor(){
+        super();
+    }
+   
+    async fetchQuestions() {
+        try {
+          const response = await fetch(`${this.apiEndpoint}/getQuestionModoCustom`);
+          const data = await response.json();
+      
+          this.questions = Object.values(data);
+          this.isLoading = false;
+      
+        } catch (error) {
+          console.error('Error fetching question data:', error);
+        }
+        return this.questions;
+      }
+    
+   
+}

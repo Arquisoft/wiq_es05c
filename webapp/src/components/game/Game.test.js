114df8d688ac76520e3272e5559e785e5fcd938e
lucia-sonia <uo282189@uniovi.es>
2024-04-17 00:43:19 +0200

    añadido un test en el game

--- webapp/src/components/game/Game.test.js
+++ webapp/src/components/game/Game.test.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import Game from './Game';
+import { BrowserRouter as Router} from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import { act } from 'react-dom/test-utils';
 
+
+// Mocked props for testing
+const mockedDarkMode = { darkMode: false };
+const mockedGameMode = { timeToAnswer: 10 }; // Assuming a default time to answer of 10 seconds
+
+// Mocked implementation for useEffect hook
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useEffect: jest.fn(),
+}));
+
+// Mocked implementation for useTranslation hook
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({ i18n: { language: 'en' } }),
+}));
 
 describe('Game component', () => {
-  it('should always pass', () => {
-    expect(true).toBe(true);
-  });
-  /*
-  it('should render correctly', () => {
+  it('renders without crashing', () => {
     render(
+      <Router>
         <ChakraProvider>
-          <Game />
+          <Game darkMode={mockedDarkMode} gameMode={mockedGameMode} />
         </ChakraProvider>
-      );
-    
-    // Verificar que el componente QuestionArea se renderice dentro de Game
-    waitFor(() => {
-        const questionAreaElement = screen.getByTestId('question-area');
-        expect(questionAreaElement).toBeInTheDocument();
-    });
+      </Router>);
   });
-  */
+
+  test('renders loading spinner when isLoading is true', () => {
+    const { getByTestId } = render(<Router>
+      <ChakraProvider>
+        <Game darkMode={mockedDarkMode} gameMode={mockedGameMode} />
+      </ChakraProvider>
+      </Router>);
+    const loadingSpinner = getByTestId('loading-spinner');
+    expect(loadingSpinner).toBeInTheDocument();
+  });
+
 });

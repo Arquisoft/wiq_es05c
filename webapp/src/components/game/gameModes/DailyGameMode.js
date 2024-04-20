6015dddab29c985414db38c9471bac0929d93959
UO283535 <uo283535@uniovi.es>
2024-04-20 16:18:06 +0200

    Añado clase daily

---
+++ webapp/src/components/game/gameModes/DailyGameMode.js
@@ -0,0 +1,56 @@
+const { default: BasicGame } = require("../BasicGame");
+//const { default: GameMode } = require("./GameMode");
+
+class DailyGameMode extends BasicGame{
+
+   
+    async fetchQuestions() {
+        try {
+          const response = await fetch(`${this.apiEndpoint}/getQuestionDiaria`);
+          const data = await response.json();
+      
+          this.questions = Object.values(data);
+          this.isLoading = false;
+      
+        } catch (error) {
+          console.error('Error fetching question data:', error);
+        }
+        return this.questions;
+      }
+
+      async sendHistory(historyData) {
+        //No se guarda la partida si no es clásica
+      }
+
+     
+      getCurrentQuestion() {
+        // Comprobar si this.questions[this.questionIndex] es undefined
+        if (this.questions[this.questionIndex] === undefined) {
+          throw new Error('No question at index ' + this.questionIndex);
+        }
+      
+        // Obtener la pregunta actual del array de preguntas
+        const data = this.questions[this.questionIndex];
+      
+        // Convertir los datos de la pregunta al formato que necesita QuestionArea
+        const questionData = {
+          pregunta: data.pregunta,
+          correcta: data.correcta,
+          respuestasIncorrecta1: data.respuestasIncorrecta1,
+          respuestasIncorrecta2: data.respuestasIncorrecta2,
+          respuestasIncorrecta3: data.respuestasIncorrecta3
+        };
+      
+        return questionData;
+      }
+
+      incrementIncorrectas(){
+        console.log("incrementa incorrectas");
+        this.incorrectas++;
+        this.finishGame();
+      }
+    
+   
+}
+
+module.exports =  DailyGameMode ;

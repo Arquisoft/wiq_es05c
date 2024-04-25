60c401c197d7b7c1e1cfefba4b4e91836f264054
lucia-sonia <uo282189@uniovi.es>
2024-04-25 20:00:42 +0200

    test e2e del infinity game pasan correctamente

--- webapp/e2e/steps/infinityMode-form.steps.js
+++ webapp/e2e/steps/infinityMode-form.steps.js
@@ -41,8 +41,8 @@ defineFeature(feature, test => {
     let passwordConfirmation;    
 
     given('An unregistered user', async () => {      
-      email = "userTestBasicGame@email.com"
-      username = "userTestBasicGame"
+      email = "userTestInfinityGame@email.com"
+      username = "userTestInfinityGame"
       password = "Contraseña_1?"
       passwordConfirmation = "Contraseña_1?"
     });
